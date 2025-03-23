1c6ae733a2284eaee8f22a7513f1dc69ddda8b73
haruna <haruna0306@g.yju.ac.kr>
2025-03-23 22:41:58 +0900

    oop

--- LearningJava/src/oop/Main.java
+++ LearningJava/src/oop/Main.java
@@ -1,38 +1,58 @@
 package oop;
 
-class Car3 {
-    static int totalCars = 0;
-    String model;
+class Calculator{
+    // 클래스 멤버 변수
+    // 모든 인스턴스에서 공유 된다
+    static int totalCalculations; // = 0;로 되지않아도 자동으로 초기값은 0
 
-    Car3(String model) {
-        this.model = model;
-        Car3.totalCars++;
+    double lastResult; // 인스턴스 멤버 변수
+
+    // 인스턴스 메소드
+    double add (double a, double b){
+          lastResult = a + b;
+          totalCalculations++;
+          return lastResult;
     }
 
-    void showModel(){
-        System.out.println("Model" + model);
+    // 인스턴스 메소드
+    double multiply(double a, double b) {
+          lastResult = a * b;
+          totalCalculations++;
+          return lastResult;
     }
 
-    static void showTotalCars(){
-        System.out.println("Total Cars: " + Car3.totalCars);
+    // 클래스 메소드 : 전체의 계산 횟수를 표시 (static이니까 )
+    static void showTotalCalculations(){
+          System.out.println("Total Calculations:: " + totalCalculations);
+    }
+
+    // 인스턴스 메소드
+    void showLastResult(){
+          System.out.println("Last Result:: " + lastResult);
     }
 }
 
 public class Main {
     public static void main(String[] args) {
-        Car3.showTotalCars();  // クラスメソッドの呼び出し（オブジェクトなし）
+        // まだ何も計算していないので0回
+        Calculator.showTotalCalculations();
 
-        Car3 car1 = new Car3("BMW");
-        Car3 car2 = new Car3("Tesla");
+        // 1つ目のインスタンス作成
+        Calculator calc1 = new Calculator();
+        calc1.add(5, 6);
+        calc1.multiply(7, 8);
+        calc1.showLastResult();
+        Calculator.showTotalCalculations();
+        System.out.println(System.identityHashCode(calc1));
+        System.out.println("\n");
 
-        car1.showModel();  // インスタンスメソッド（オブジェクトを通じて呼び出し）
-        car2.showModel();
-
-        Car3.showTotalCars(); // クラスメソッドの再呼び出し（オブジェクトなし）
-
-
-
-
+        // 2つ目のインスタンス作成
+        Calculator calc2 = new Calculator();
+        calc2.add(3, 9);
+        calc2.multiply(10, 2);
+        calc2.showLastResult();
+        Calculator.showTotalCalculations();
+        System.out.println(System.identityHashCode(calc2));
 
     }
 }
