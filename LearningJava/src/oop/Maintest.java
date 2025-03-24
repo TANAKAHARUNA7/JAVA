46b4d2e562df6e67b7f4654375bff020fdf23467
haruna <haruna0306@g.yju.ac.kr>
2025-03-24 23:30:56 +0900

    oop

--- LearningJava/src/oop/Maintest.java
+++ LearningJava/src/oop/Maintest.java
@@ -1,82 +1,38 @@
 package oop;
+    class Car2{
+        static int totalCars;
+        String model;
 
-class Std{
 
-    int korean;
-    int math;
-    int english;
-    int sum;
-    final int NUM_OF_SUBJECT = 3;
+        static{
+        System.out.println("初期化ブロック");}
 
-    int id;
-    String name;
+        Car2(String model){
+            this.model = model;
+            System.out.println("モデル生成！");
+            totalCars++;
+        }
 
-    void setGrade(int argKorean, int argMath, int argEnglish){
-        korean = argKorean;
-        math = argMath;
-        english = argEnglish;
-        sum = korean + math + english;
+        void showModel(){
+            System.out.println("Model: " + model);
+        }
+
+        static void showTotalCars(){
+            System.out.println("Total Cars: " + totalCars);
+        }
     }
-
-    void prtStu(){
-     System.out.println("国語" + korean + "点," + "数学" + math + "点,"
-             + "英語" + english + "点," + "合計" + sum + "点");
-    }
-
-    double getSum(){
-        return sum;
-    }
-    int getid(){
-        return id;
-    }
-
-    String getName(){
-        return name;
-    }
-
-    double getAvg(){
-        return sum / NUM_OF_SUBJECT;
-    }
-}
-
-class Car1{
-    String brand;
-
-    void prtCar() {
-        System.out.println("brand: " + brand);
-    }
-}
-
-class Bar{
-    String name;
-    int age;
-
-    // 製造者 -> クラスの名前と同一でなければならない
-    Bar(String argName, int argAge) {
-        name = argName;
-        age = argAge;
-//        System.out.println("Bar 製造者放出");
-    }
-
-    void prtBar(){
-        System.out.println(name + "\t" + age + "才");
-    }
-}
-
 
 public class Maintest {
     public static void main(String[] args) {
-        Std stu1 = new Std();
-        stu1.name = "John";
-        stu1.setGrade(80, 90, 70);
-        stu1.prtStu();
+        Car2.showTotalCars();
 
-        Car1 car1 = new Car1();
-        car1.brand = "SUZUKI";
-        car1.prtCar();
+        Car2 car1 = new Car2("TOYOTA");
 
-        Bar b1 = new Bar("Richard", 25);
-        b1.prtBar();
+        Car2 car2 = new Car2("SUZUKI");
 
+        car1.showModel();
+        car2.showModel();
+
+        Car2.showTotalCars();
     }
 }
--- LearningJava/src/oop/Test2.java
+++ LearningJava/src/oop/Test2.java
@@ -1,5 +1,23 @@
 package oop;
 
-public class Test2 {
+class Student25 {
+    String name;
+    int age;
 
+    Student25(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    void prt(){
+        System.out.println(name + " " + age);
+    }
+}
+
+
+public class Test2 {
+    public static void main(String[] args) {
+        Student25 st1 = new Student25("하루나",30);
+        st1.prt();
+    }
 }
