cfba803137933843381102c4371bc8aeabe91027
haruna <haruna0306@g.yju.ac.kr>
2025-04-13 21:01:08 +0900

    "OOP練習"

--- LearningJava/src/SelfStudy/Day_4_13.java
+++ LearningJava/src/SelfStudy/Day_4_13.java
@@ -1,37 +1,100 @@
 package SelfStudy;
 
-class Dog{
-    private String name;
-    private int age;
+import java.util.Scanner;
 
-    {System.out.println("Dog instance");}
+// 학생 정보를 관리하는 class
+class Stds {
+    // ID, 이름, 국어, 영어, 수학, 합계점수, 평균
+    int id;
+    String name;
+    int scoreNational;
+    int scoreMath;
+    int scoreEng;
+    int sum;
+    float avg;
 
-    public Dog(String name, int age){
-        this.name = name;
-        this.age = age;
+    Stds studentList;
+
+    void setScore(int argNational, int argMath, int argEng){
+        // 정수 0 ~ 100 이위는 오류
+        if (argNational < 0 || argMath < 0 || argEng < 0
+                || argNational > 100 || argMath > 100 || argEng > 100){
+            System.out.println("입력이 올바르지 않습니다.");
+            return;
+        }
+
+        scoreNational = argNational;
+        scoreMath = argMath;
+        scoreEng = argEng;
+
     }
 
-    public Dog(int age ){
-        this("A dog", age);
+
+
+
+}
+// 학생 정보 입력을 받는 class
+class StdMng extends Stds {
+
+    Scanner sc = new Scanner(System.in);
+
+    // 입력 저리
+    public void input(){
+        System.out.println("국어: ");
+        int national = sc.nextInt();
+
+        System.out.println("수학: ");
+        int math = sc.nextInt();
+
+        System.out.println("영어: ");
+        int english = sc.nextInt();
+
+        super.setScore(national, math, english);
+
+
+
+
     }
 
-    public Dog(){
-        this("A dog", 0);
-    }
-
-    public String profile(){
-        return name + "("+age+")";
-    }
 
 }
 
 public class Day_4_13 {
     public static void main(String[] args) {
-    Dog black = new Dog(2);
-    System.out.println(black.profile());
-    System.out.println("");
+        // 학생의 성적을 받고 배열에 저장, 출력하는 프로그램
+        Scanner sc =new Scanner(System.in);
+        Stds stu;
 
-    Dog white = new Dog();
-    System.out.println(white.profile());
-    }
+        while(true){
+            // menu를 출력한다
+            System.out.print("======== menue ========");
+            System.out.println("\n1. 입력");
+            System.out.println("\n2. 출력");
+            System.out.println("\n3. 프로그램 종료");
+
+            int choice = sc.nextInt();
+            // menu에 따라 프로그램 실행
+            // 3. 프로그램 종료
+            if(choice == 3){
+                System.out.println("프로그램 종료");
+                break;
+            }
+
+            switch(choice) {
+                // 1. 입력
+                case 1:
+                 // 입력 수 받기
+                    System.out.println("입력 수 를 입력하세요: ");
+                    int stuNum = sc.nextInt();
+
+
+
+                // 2. 출력
+            }
+
+        }
+
+
+
+}
 }
