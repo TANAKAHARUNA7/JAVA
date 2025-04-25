a38ebf03d205a9841fd263d8a9e004e837ab1f46
haruna <haruna0306@g.yju.ac.kr>
2025-04-25 23:56:16 +0900

    "OOP継承"

--- LearningJava/src/Practice_Day/Day_4_9_No2.java
+++ LearningJava/src/Practice_Day/Day_4_9_No2.java
@@ -1,38 +1,32 @@
 package Practice_Day;
 
-abstract class Player {
+abstract class Player2 {
     void shoot() { System.out.println("슛");}
 }
-class Sg extends Player {
+class Sg2 extends Player2 {
     void shoot() { System.out.println("슈팅가드 슛");}
 }
-class Pg extends Player {
+class Pg2 extends Player2 {
     void shoot() { System.out.println("포인트 슛");}
 }
-class C3 extends Player {
+class C2 extends Player2 {
     void shoot() { System.out.println("센터 슛");}
 }
-class Pf extends Player {
+class Pf2 extends Player2 {
     void shoot() { System.out.println("파워포워드 슛");}
 }
-class Sf extends Player {
+class Sf2 extends Player2 {
     void shoot() { System.out.println("스몰포워드 슛");}
 }
 public class Day_4_9_No2 {
     public static void main(String[] args) {
 
-//        C c = new C();
-//        Sg sg = new Sg();
-//        Pg pg = new Pg();
-//        Sf sf = new Sf();
-//        Pf pf = new Pf();
-
-        Player p[] = new Player[5];
-        p[0] = new C3();
-        p[1] = new Sg();
-        p[2] = new Pg();
-        p[3] = new Sf();
-        p[4] = new Pf();
+        Player2 p[] = new Player2[5];
+        p[0] = new C2();
+        p[1] = new Sg2();
+        p[2] = new Pg2();
+        p[3] = new Sf2();
+        p[4] = new Pf2();
 
         for (int i = 0; i < 5; i++) {
             p[i].shoot();
